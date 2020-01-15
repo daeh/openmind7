5f65df53f8b7f0a781ad0562bb01880c25bfdb32
Dae Houlihan <daeda@mit.edu>
2020-01-15 16:27:33 -0500

    new node path

--- update_shell_env.sh
+++ update_shell_env.sh
@@ -2,6 +2,11 @@ source ~/.zshrc || exit
 
 tb_conda
 
+zsh --version
+echo "current tmux:"
+tmux -V
+echo "update via https://gist.github.com/ryin/3106801"
+
 upgrade_oh_my_zsh
 
 cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || exit
@@ -15,20 +20,29 @@ git pull --rebase --stat origin master
 
 cd ~ || exit
 
-echo "current tmux:"
-tmux -V
-echo "update via https://gist.github.com/ryin/3106801"
-
 conda update -n base -c defaults conda
 conda update -n base -c defaults python
 
 conda update -n omlab -c anaconda anaconda
 conda update -n omlab -c anaconda python
+conda update -n omlab --all -c anaconda
 
 pip install --upgrade pip
 pip install -U tmuxp
 
-env_wppl
+env_iaa
 conda update -c anaconda anaconda
+conda update -c anaconda python
+conda update --all -c pytorch -c anaconda
+pip install --upgrade pip
+pip install -U tmuxp
+
+node -v
+##### https://johnpapa.net/node-and-npm-without-sudo/
+# Install Node.js from https://nodejs.org/en/download/
+#####
+
 npm install -g npm
+npm update -g jshint
 npm update -g webppl
+npm install --prefix ~/.webppl webppl-json --force
